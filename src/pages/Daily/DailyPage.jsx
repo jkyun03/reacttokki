346ec5fbaf79d3b90eddc079f32ed793dbfab658
hyroh5 <hyrlove5598@khu.ac.kr>
2026-04-13 11:39:09 +0900

    파일 구조 수정

--- src/pages/Daily/DailyPage.jsx
+++ src/pages/Daily/DailyPage.jsx
@@ -1,18 +1,21 @@
 import TopTabs from "../../components/common/TopTabs";
 import GlassPanel from "../../components/common/GlassPanel";
 import backgroundImg from "../../assets/background.png";
+import "./Daily.css"
 
 export default function Daily() {
   return (
     <div
       className="daily-page"
-      style={{ backgroundImage: `url(${backgroundImg})`}}
+      style={{ 
+        backgroundImage: `url(${backgroundImg})`,
+      }}
     >
 
       <TopTabs />
       
       <GlassPanel className="daily-panel"></GlassPanel>
-      
+
     </div>
 
   )
